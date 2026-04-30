380ac908cc73d328a06d89b42bc51914d155fb04
Rowan Border <rowanjborder@gmail.com>
2026-04-30 20:51:52 +0300

    clean up arming code

--- src/api.cpp
+++ src/api.cpp
@@ -740,17 +740,6 @@ std::tuple<bool, std::string> MrsUavPx4Api::callbackArming(
 
   auto srv_out = std::make_shared<mavros_msgs::srv::CommandLong::Request>();
 
-  // when REALWORLD AND ARM:=TRUE
-  // if (!_simulation_ && request) {
-  //   ss << "can not arm by service when not in simulation! You should arm the
-  //   "
-  //         "drone by the RC controller only!";
-  //   RCLCPP_ERROR_STREAM_THROTTLE(node_->get_logger(), *clock_, 1000,
-  //                                "" << ss.str());
-
-  //   return {false, ss.str()};
-  // }
-
   srv_out->broadcast = false;
   srv_out->command = 400;  // the code for arming
   srv_out->confirmation = true;
